ec38f79890d9e9f827d3de7ba5cf37ef2face46c
DrRobotikk <tangenrobin@gmail.com>
2023-04-29 23:09:04 +0200

    small update

--- tictactoe/src/App.js
+++ tictactoe/src/App.js
@@ -30,28 +30,26 @@ function Board({ xIsNext, squares, onPlay }) {
     status = "Next player: " + (xIsNext ? "X" : "O");
   }
 
+  const rows = 3;
+  const cols = 3;
+
   return (
-    <>
+    <div>
       <div className="status">{status}</div>
-      <div className="board-row">
-        <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
-        <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
-        <Square value={squares[2]} onSquareClick={() => handleClick(2)} />
-      </div>
-      <div className="board-row">
-        <Square value={squares[3]} onSquareClick={() => handleClick(3)} />
-        <Square value={squares[4]} onSquareClick={() => handleClick(4)} />
-        <Square value={squares[5]} onSquareClick={() => handleClick(5)} />
-      </div>
-      <div className="board-row">
-        <Square value={squares[6]} onSquareClick={() => handleClick(6)} />
-        <Square value={squares[7]} onSquareClick={() => handleClick(7)} />
-        <Square value={squares[8]} onSquareClick={() => handleClick(8)} />
-      </div>
-    </>
+      {[...Array(3)].map((_, i) => (
+        <div key={i} className="board-row">
+          {[...Array(cols)].map((_, j) => (
+            <Square
+              key={i * cols + j}
+              value={squares[i * 3 + j]}
+              onSquareClick={() => handleClick(i * 3 + j)}
+            />
+          ))}
+        </div>
+      ))}
+    </div>
   );
 }
-
 export default function Game() {
   const [history, setHistory] = useState([Array(9).fill(null)]);
   const [currentMove, setCurrentMove] = useState(0);
